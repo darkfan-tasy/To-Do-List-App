c1b9225dfea1f734d98f8e455c1a2c2e3e54d5bd
Anupam Agarwal <anupamagarwal1912@gmail.com>
2022-02-22 17:40:47 +0530

    my commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Todo list',
       home: new TodoList(),
     );
